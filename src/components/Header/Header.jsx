5488a65065512e6f49e95afda2cd025520253286
devgiack <devgiack@protonmail.com>
2023-03-08 14:47:58 +0100

    Creation du header + correction du CSS de la page home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,20 @@
-import './Header.css';
-import ConnectWalletButton from '../ConnectWalletButton/ConnectWalletButton';
+import "./Header.css";
+import ConnectWalletButton from "../ConnectWalletButton/ConnectWalletButton";
+import logo from "../../assets/images/LOGO-TPH.png";
 
 const Header = () => (
-    <div className="main-header">
-        <div className='connect'>
+    <div className="header-container">
+            <img className="logo" src={logo} />
+        <div className="header-content">
+            <h3 className="link">Home</h3>
+            <h3 className="link">About</h3>
+            <h3 className="link">Roadmap</h3>
+            <h3 className="link">Team</h3>
+            <h3 className="link">FAQ</h3>
+        </div>
+        <div className="button-connect">
             <ConnectWalletButton />
         </div>
-       
     </div>
 );
 
