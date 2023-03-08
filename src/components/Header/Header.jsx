fe699baeab219359d2ccb4ac07a084c1f3884f9d
devgiack <devgiack@protonmail.com>
2023-03-08 17:43:39 +0100

    Suppresion bouton de deconnexion du header + CSS modif

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,22 @@
 import "./Header.css";
 import ConnectWalletButton from "../ConnectWalletButton/ConnectWalletButton";
 import logo from "../../assets/images/LOGO-TPH.png";
-import DisconnectWalletButton from '../DisconnectWalletButton/DisconnectWalletButton'
-
-import { UserAddressAtom } from '../atom/UserWalletAddress'
-import { useAtom } from 'jotai';
 
 const Header = () => {
-
-    const [userAddress] = useAtom(UserAddressAtom);
-
     return (
         <div className="header-container">
             <img className="logo" src={logo} />
-        <div className="header-content">
-            <h3 className="link">Home</h3>
-            <h3 className="link">About</h3>
-            <h3 className="link">Roadmap</h3>
-            <h3 className="link">Team</h3>
-            <h3 className="link">FAQ</h3>
+            <div className="header-content">
+                <h3 className="link">Home</h3>
+                <h3 className="link">About</h3>
+                <h3 className="link">Roadmap</h3>
+                <h3 className="link">Team</h3>
+                <h3 className="link">FAQ</h3>
+            </div>
+            <div className="button-connect">
+                <ConnectWalletButton />
+            </div>
         </div>
-        <div className="button-connect">
-
-            <ConnectWalletButton />
-            <DisconnectWalletButton/>
-            <p style={{ color: 'white' }}>{userAddress}</p>
-        </div>
-    </div>
     );
 };
 
