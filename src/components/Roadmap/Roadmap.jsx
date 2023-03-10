6b2c1bddf051967249c0759463724622ae6854ac
YannGUIOT <guiot_yann@hotmail.fr>
2023-03-10 16:29:21 +0100

    titles & folder Roadmap + Show + Homepage-update

---
+++ src/components/Roadmap/Roadmap.jsx
@@ -0,0 +1,14 @@
+import "./Roadmap.css"
+
+export const Roadmap = () => {
+
+  return (
+    <>
+      <div className="bloc-title">
+        <span id="big-title">TIMELINE</span>
+        <span id="little-title">Roadmap</span>
+        <span id="title-border"></span>
+      </div>
+    </>
+  )
+}
---
+++ src/components/Show/Show.jsx
@@ -0,0 +1,14 @@
+import "./Show.css"
+
+export const Show = () => {
+
+  return (
+    <>
+      <div className="bloc-title">
+        <span id="big-title">SHOWCASE</span>
+        <span id="little-title">Our Collections</span>
+        <span id="title-border"></span>
+      </div>
+    </>
+  )
+}
