82e4221c3a9527a3e43b3692a9c809ed90590b9a
YannGUIOT <guiot_yann@hotmail.fr>
2023-03-10 12:13:32 +0100

    Provisory FAQ-component + rectifs css

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,23 +3,25 @@ import "./About.css"
 export const About = () => {
 
   return (
-    <div className="global-about">
-      <div className="part_1-about">
+    <>
+      <div className="bloc-title">
         <span id="big-title">ABOUT US</span>
-        <span id="title">Our Story</span>
+        <span id="little-title">Our Story</span>
         <span id="title-border"></span>
       </div>
-      <div className="part_2-about">
-        <div className="part_2A-about">
-          <img src="../src/assets/images/PACK_2.png" alt="pack2_NFT" />
+      <div className="global-about">
+        <div className="bloc-about">
+          <div className="blocA-about">
+            <img src="../src/assets/images/PACK_2.png" alt="pack2_NFT" />
+          </div>
+          <div className="blocB-about">
+            <span id="title-about">Welcome to The Packing Hroject Art Studio</span>
+            <span id="text1">The Packing Hroject Art Studio is a private collection of NFT's unique digital collectibles. Lorem ipsum is simply dummy text of the printing and typesetting industry.</span>
+            <span id="text2">Lorem lipusus poil à l'anus is an over text pour meubler. Ce site est une bombe atomique, ça va envoyer du lourd au passage devant les jury qui vont en prendre des leçons.</span>
+            <button className="discord_button">Join Our Discord &nbsp; &#62;</button>
+          </div> 
         </div>
-        <div className="part_2B-about">
-          <span id="title-about">Welcome to The Packing Hroject Art Studio</span>
-          <span id="text1">The Packing Hroject Art Studio is a private collection of NFT's unique digital collectibles. Lorem ipsum is simply dummy text of the printing and typesetting industry.</span>
-          <span id="text2">Lorem lipusus poil à l'anus is an over text pour meubler. Ce site est une bombe atomique, ça va envoyer du lourd au passage devant les jury qui vont en prendre des leçons.</span>
-          <button className="discord_button">Join Our Discord &nbsp; &#62;</button>
-        </div> 
       </div>
-    </div>
+    </>
   )
 }
