36f5de0ac25c4325c0aad342af564fc18a346f35
devgiack <devgiack@protonmail.com>
2023-03-10 12:41:48 +0100

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import { Intro } from "../components/Intro/Intro";
 import { About } from "../components/About/About";
 import { Team } from "../components/Team/Team";
-
 import { Faq } from "../components/Faq/Faq";
 
 export const Home = () => {
