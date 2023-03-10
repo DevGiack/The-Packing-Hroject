66b34f0022efa2c7e4f97128577e7841cf78770d
adeloing <adeloing@gmail.com>
2023-03-10 01:26:13 +0100

    ajout de la gallerie perso

--- src/components/OwnCollection/OwnCollection.jsx
+++ src/components/OwnCollection/OwnCollection.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import './OwnCollection.css';
 import { Network, Alchemy } from 'alchemy-sdk';
 import { v1 as uuidv1 } from 'uuid';
-import { useAtom } from "jotai";
+import { atom, useAtom } from 'jotai';
 import { UserAddressAtom } from "../atom/UserWalletAddress";
+
 const settings = {
   apiKey: import.meta.env.VITE_MORALIS_API_KEY,
   network: Network.ETH_MAINNET,
 };
 const alchemy = new Alchemy(settings);
-//const nftsForOwner = await alchemy.nft.getNftsForOwner(UserAddressAtom);
-const nftsForOwner = await alchemy.nft.getNftsForOwner('0x5F8E0020C3164fB7EB170D7345672F6948Ca0FF4');
+const nftsForOwner = await alchemy.nft.getNftsForOwner('0x1Fa8a823bbaEAa87bd9aaF0eCb451d1b165254d4');
 let contractName = '';
 let NFTTitle = '';
 let imageUrl = '';
@@ -21,7 +21,6 @@ const OwnCollection = () => {
   return (<div className="OwnCollection">
     {
       nftsForOwner.ownedNfts.map((x) => {
-        console.log(x);
           x.tokenId ? x.tokenId.length > 6 ? tokenId = `${x.tokenId.slice(0, 6)}...` : tokenId = x.tokenId : tokenId = 'undefined';
           x.contract.name ? x.contract.name.length > 20 ? contractName = `${x.contract.name.slice(0, 16)}...` : contractName = x.contract.name : contractName = 'undefined';
           x.title ? x.title.length > 20 ? NFTTitle = `${x.title.slice(0, 16)}...` : NFTTitle = x.title : NFTTitle = 'undefined';
