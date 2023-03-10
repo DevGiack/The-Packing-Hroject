800c7bce7dc1742ffd976a5516bcc3bacc5369dc
devgiack <devgiack@protonmail.com>
2023-03-10 12:29:43 +0100

    header dynamique en position fixe + création des ancres

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,81 @@
-import "./Header.css";
-import ConnectWalletButton from "../ConnectWalletButton/ConnectWalletButton";
-import logo from "../../assets/images/LOGO-TPH.png";
-
+import "./Header.css"
+import { useState, useEffect } from "react"
+import ConnectWalletButton from "../ConnectWalletButton/ConnectWalletButton"
+import logo from "../../assets/images/LOGO-TPH.png"
 
 export const Header = () => {
+    const [activeLink, setActiveLink] = useState("home")
+
+    function handleLinkClick(linkId) {
+        setActiveLink(linkId)
+
+        function handleScroll() {
+            const sections = ["home", "about", "roadmap", "team", "faq"]
+            const currentScrollPos = window.pageYOffset
+
+            for (let i = sections.length - 1; i >= 0; i--) {
+                const element = document.getElementById(sections[i])
+                const { top, bottom } = element.getBoundingClientRect()
+
+                if (currentScrollPos >= top && currentScrollPos < bottom) {
+                    setActiveLink(sections[i])
+                    break
+                }
+            }
+        }
+
+        useEffect(() => {
+            window.addEventListener("scroll", handleScroll)
+            return () => {
+                window.removeEventListener("scroll", handleScroll)
+            }
+        }, [])
+    }
     return (
         <div className="header-container">
             <img className="logo" src={logo} />
             <div className="header-content">
-                <h3 className="link">Home</h3>
-                <h3 className="link">About</h3>
-                <h3 className="link">Roadmap</h3>
-                <h3 className="link">Team</h3>
-                <h3 className="link">FAQ</h3>
+                <a
+                    className={`link ${activeLink === "home" ? "active" : ""}`}
+                    href="#home"
+                    onClick={() => handleLinkClick("home")}
+                >
+                    Home
+                </a>
+                <a
+                    className={`link ${activeLink === "about" ? "active" : ""}`}
+                    href="#about"
+                    onClick={() => handleLinkClick("about")}
+                >
+                    About
+                </a>
+                <a
+                    className={`link ${
+                        activeLink === "roadmap" ? "active" : ""
+                    }`}
+                    href="#roadmap"
+                    onClick={() => handleLinkClick("roadmap")}
+                >
+                    Roadmap
+                </a>
+                <a
+                    className={`link ${activeLink === "team" ? "active" : ""}`}
+                    href="#team"
+                    onClick={() => handleLinkClick("team")}
+                >
+                    Team
+                </a>
+                <a
+                    className={`link ${activeLink === "faq" ? "active" : ""}`}
+                    href="#faq"
+                    onClick={() => handleLinkClick("faq")}
+                >
+                    FAQ
+                </a>
             </div>
             <div className="button-connect">
                 <ConnectWalletButton />
             </div>
         </div>
-    );
-};
-
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,26 @@ import { Intro } from "../components/Intro/Intro";
 import { About } from "../components/About/About";
 import { Team } from "../components/Team/Team";
 
+
 export const Home = () => {
 
   return (
     <>
+    <div id="home">
       <Intro />
+      </div>
+      <div id="about">
       <About />
+      </div>
+      <div id="roadmap">
+        
+      </div>
+      <div id="team">
       <Team />
+      </div>
+      <div id="faq">
+
+      </div>
     </>
   )
 }
