f911711f8bd512db13ab8986ab9d50df84528c02
YannGUIOT <guiot_yann@hotmail.fr>
2023-03-13 16:05:40 +0100

    update header & margin of #header links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,11 +5,13 @@ import ConnectWalletButton from "../ConnectWalletButton/ConnectWalletButton"
 import logo from "../../assets/images/LOGO-TPH.png"
 
 export const Header = () => {
+
     const [activeLink, setActiveLink] = useState(null);
     
     const handleClick = (event) => {
-        setActiveLink(event.target.hash);
-      };
+      setActiveLink(event.target.hash);
+    };
+
 return (
     <div className="header-container">
       <Link to="/profil">
@@ -17,40 +19,35 @@ return (
         </Link>
         <div className="header-content">
         <a
-  className={`link ${activeLink === "#home" ? "active" : ""}`}
-  href="#home"
-  onClick={handleClick}
->
-  Home
-</a>
-<a
-  className={`link ${activeLink === "#about" ? "active" : ""}`}
-  href="#about"
-  onClick={handleClick}
->
-  About
-</a>
-<a
-  className={`link ${activeLink === "#roadmap" ? "active" : ""}`}
-  href="#roadmap"
-  onClick={handleClick}
->
-  Roadmap
-</a>
-<a
-  className={`link ${activeLink === "#team" ? "active" : ""}`}
-  href="#team"
-  onClick={handleClick}
->
-  Team
-</a>
-<a
-  className={`link ${activeLink === "#faq" ? "active" : ""}`}
-  href="#faq"
-  onClick={handleClick}
->
-  FAQ
-</a>
+          className={`link ${activeLink === "#home" ? "active" : ""}`}
+          href="#home"
+          onClick={handleClick}
+        >Home
+        </a>
+        <a
+          className={`link ${activeLink === "#about" ? "active" : ""}`}
+          href="#about"
+          onClick={handleClick}
+        >About
+        </a>
+        <a
+          className={`link ${activeLink === "#show" ? "active" : ""}`}
+          href="#show"
+          onClick={handleClick}
+        >Collection
+        </a>
+        <a
+          className={`link ${activeLink === "#team" ? "active" : ""}`}
+          href="#team"
+          onClick={handleClick}
+        >Team
+        </a>
+        <a
+          className={`link ${activeLink === "#faq" ? "active" : ""}`}
+          href="#faq"
+          onClick={handleClick}
+        >FAQ
+        </a>
         </div>
         <div className="button-connect">
             <ConnectWalletButton />
