15fd30722ddb9592a1c8431beabb5b24c9dc820e
YannGUIOT <guiot_yann@hotmail.fr>
2023-03-14 17:28:09 +0100

    update intro & about texts

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -16,8 +16,8 @@ export const About = () => {
           </div>
           <div className="blocB-about">
             <span id="title-about">Welcome to The Packing Hroject Art Studio</span>
-            <span id="text1">The Packing Hroject Art Studio is a private collection of NFT's unique digital collectibles. Lorem ipsum is simply dummy text of the printing and typesetting industry.</span>
-            <span id="text2">Lorem lipusus poil à l'anus is an over text pour meubler. Ce site est une bombe atomique, ça va envoyer du lourd au passage devant les jury qui vont en prendre des leçons.</span>
+            <span id="text1">The Packing Hroject Art Studio is a private collection of NFT's unique digital collectibles. These images are the products of hard work and the fruit of an overflowing imagination.</span>
+            <span id="text2">The tedious production of these works of art is the result of hundreds of hours of artistic work. We hope you like this particular collection and make you want to participate in its success.</span>
             <button className="discord_button">Join Our Discord &nbsp; &#62;</button>
           </div> 
         </div>
