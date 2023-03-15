cefb2ee87b747f1222ec95d161f201904bac3407
adeloing <adeloing@gmail.com>
2023-03-15 01:45:23 +0100

    ok

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,26 +7,28 @@
 import hre from "hardhat";
 
 async function main() {
-  const SimpleStorageFactory = await hre.ethers.getContractFactory("SimpleStorage")
+  const tphFactory = await hre.ethers.getContractFactory("TPH")
   console.log("Deploying contract...")
-  const simpleStorage = await SimpleStorageFactory.deploy()
-  await simpleStorage.deployed()
-  console.log(`Deployed contract to: ${simpleStorage.address}`)
+  const tph = await tphFactory.deploy()
+  await tph.deployed()
+  console.log(`Deployed contract to: ${tph.address}`)
   // what happens when we deploy to our hardhat network?
   if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
     console.log("Waiting for block confirmations...")
-    await simpleStorage.deployTransaction.wait(6)
-    await verify(simpleStorage.address, [])
+    await tph.deployTransaction.wait(6)
+    await verify(tph.address, [])
   }
 
-  const currentValue = await simpleStorage.retrieve()
-  console.log(`Current Value is: ${currentValue}`)
-
-  // Update the current value
-  const transactionResponse = await simpleStorage.store(7)
-  await transactionResponse.wait(1)
-  const updatedValue = await simpleStorage.retrieve()
-  console.log(`Updated Value is: ${updatedValue}`)
+  const mint = await tph.safeMint("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+  let test = await tph.getCounter();
+  console.log(`mint du NFT #${test}`);
+//  let owner = await tph.ownerOf("1");
+//  console.log(`owner: ${owner}`);
+  const mint2 = await tph.safeMint("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+  let test2 = await tph.getCounter();
+  console.log(`${test2}`);
+//  let owner2 = await tph.ownerOf("2");
+//  console.log(`owner: ${owner2}`);
 }
 
 // async function verify(contractAddress, args) {
