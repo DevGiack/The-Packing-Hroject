7ccd9abbcfac6231b1f3e18727394d63c202da86
YannGUIOT <guiot_yann@hotmail.fr>
2023-03-16 17:35:24 +0100

    button code retouch

--- src/components/MintButton/MintButton.jsx
+++ src/components/MintButton/MintButton.jsx
@@ -1,12 +1,11 @@
-import React from "react"
 import "./MintButton.css";
 
 export const MintButton = () => {
     return (
-        <div class="container">
-            <div class="btn">
+        <div className="container">
+            <div className="btn">
                 <span>MINT NOW</span>
-                <div class="dot"></div>
+                <div className="dot"></div>
             </div>
         </div>
     )
