98f59b80a61ae565e405db4aeb92e687222f29b9
adeloing <adeloing@gmail.com>
2023-03-16 16:51:37 +0100

    minors modifications

--- src/components/OwnCollection/OwnCollection.jsx
+++ src/components/OwnCollection/OwnCollection.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import './OwnCollection.css';
 import { Network, Alchemy } from 'alchemy-sdk';
 import { v1 as uuidv1 } from 'uuid';
-import { atom, useAtom } from 'jotai';
-import { UserAddressAtom } from "../atom/UserWalletAddress";
+
+const network = {"80001": "polygon-mumbai",
+"137": "polygon-mainnet", "1": "eth-mainnet"
+}
 
 const settings = {
   apiKey: import.meta.env.VITE_MORALIS_API_KEY,
-  network: Network.MATIC_MUMBAI,
+  network: network[window.ethereum.networkVersion],
 };
 const alchemy = new Alchemy(settings);
-const nftsForOwner = await alchemy.nft.getNftsForOwner('0xb3faf97A9A9f6C0a54C6ddFDC4a188E702a45205');
+const nftsForOwner = await alchemy.nft.getNftsForOwner(window.ethereum.selectedAddress);
 
 let contractName = '';
 let NFTTitle = '';
@@ -30,11 +32,11 @@ export const OwnCollection = () => {
     </div>
     <div className="bloc-cards">
     {
-      nftsForOwner.ownedNfts.map((x) => {
+      nftsForOwner.ownedNfts.filter(obj => obj.title == "THE PACKING HROJECT").map((x) => {
           x.tokenId ? x.tokenId.length > 6 ? tokenId = `${x.tokenId.slice(0, 6)}...` : tokenId = x.tokenId : tokenId = 'undefined';
           x.contract.name ? x.contract.name.length > 20 ? contractName = `${x.contract.name.slice(0, 16)}...` : contractName = x.contract.name : contractName = 'undefined';
           x.title ? x.title.length > 20 ? NFTTitle = `${x.title.slice(0, 16)}...` : NFTTitle = x.title : NFTTitle = 'undefined';
-          x.media[0].thumbnail ? imageUrl = x.media[0].thumbnail: imageUrl = 'undefined';
+          x.media[0].gateway ? imageUrl = x.media[0].gateway : imageUrl = 'undefined';
         //  x.contract.openSea.floorPrice ? OPFloor = x.contract.openSea.floorPrice : OPFloor = 'undefined';
           const id = uuidv1();
           return (
@@ -42,9 +44,8 @@ export const OwnCollection = () => {
               <div className="card-body">
                 <span className="card-id"># {tokenId}</span>
                 <img src={imageUrl} className="card-img" alt="..."></img>
-                <span>{contractName}</span>
                 <span>{NFTTitle}</span>
-                <span className="cardtxt">{OPFloor} ETH</span>
+                <span><b>member {tokenId}/50</b></span>
                 <div className="card-button-link">
                   <button id="details_button">Details</button>
                 </div>
