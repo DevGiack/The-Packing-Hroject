e9234e1eb1973bdb1604d756484d545e1fc83be0
YannGUIOT <guiot_yann@hotmail.fr>
2023-03-16 16:53:31 +0100

    adjust conditions css

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -7,43 +7,47 @@ export const Terms = () => {
         window.scrollTo(0, 0);
       });
     return (
+        <>
+        <div className="bloc-title">
+            <span id="big-title">TERMS & CONDITIONS</span>
+            <span id="little-title">Terms & Conditions</span>
+            <span id="title-border"></span>
+        </div>
         <div className="bloc">
-            <h1 id="TC">Terms and Conditions</h1>
-
-            <p>
+            <div className="paragraph">
                 Welcome to The Packing Hroject! By accessing or using our
                 website, you agree to comply with and be bound by the following
                 terms and conditions:
-            </p>
+            </div>
 
             <h2>Use of Site:</h2>
-            <p>
+            <div className="paragraph">
                 You may use our site for lawful purposes only. You may not use
                 our site in any way that violates any applicable laws or
                 regulations, infringes upon the rights of others, or is harmful,
                 threatening, or abusive.
-            </p>
+            </div>
 
             <h2>Intellectual Property:</h2>
-            <p>
+            <div className="paragraph">
                 The content on our website, including text, graphics, logos,
                 images, and software, is the property of The Packing Project and
                 is protected by copyright and other intellectual property laws.
                 You may not use, reproduce, or distribute any of our content
                 without our express written permission.
-            </p>
+            </div>
 
             <h2>Third-Party Links:</h2>
-            <p>
+            <div className="paragraph">
                 Our website may contain links to third-party websites that are
                 not owned or operated by The Packing Project. We do not endorse
                 or assume any responsibility for the content, privacy policies,
                 or practices of any third-party websites. You access these sites
                 at your own risk.
-            </p>
+            </div>
 
             <h2>Disclaimer of Warranties:</h2>
-            <p>
+            <div className="paragraph">
                 Our website is provided on an "as is" and "as available" basis.
                 We make no warranties or representations of any kind, whether
                 express or implied, regarding the operation of our site, the
@@ -51,43 +55,44 @@ export const Terms = () => {
                 site, or the functionality of our site. We disclaim all
                 warranties, including but not limited to implied warranties of
                 merchantability and fitness for a particular purpose.
-            </p>
+            </div>
 
             <h2>Limitation of Liability:</h2>
-            <p>
+            <div className="paragraph">
                 The Packing Hroject shall not be liable for any damages of any
                 kind arising from the use of our website, including but not
                 limited to direct, indirect, incidental, punitive, and
                 consequential damages. In no event shall our liability exceed
                 the amount paid by you, if any, for accessing our site.
-            </p>
+            </div>
 
             <h2>Indemnification:</h2>
-            <p>
+            <div className="paragraph">
                 You agree to indemnify and hold harmless The Packing Project,
                 its officers, directors, employees, and agents from any and all
                 claims, damages, losses, liabilities, and expenses (including
                 attorneys' fees) arising from your use of our website or your
                 violation of these terms and conditions.
-            </p>
+            </div>
 
             <h2>Changes to Terms:</h2>
-            <p>
+            <div className="paragraph">
                 We reserve the right to modify these terms and conditions at any
                 time without prior notice. By continuing to use our site after
                 we post any such changes, you accept the modified terms and
                 conditions.
-            </p>
+            </div>
 
             <h2>Contact Us:</h2>
-            <p>
+            <div className="paragraph">
                 If you have any questions or concerns about our terms and
                 conditions, please contact us at{" "}
                 <a href="mailto:info@thepackingproject.com">
                     info@thepackingproject.com
                 </a>
                 .
-            </p>
+            </div>
         </div>
+        </>
     )
 }
