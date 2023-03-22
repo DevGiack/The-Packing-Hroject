6750fee5104a04db360438d23c5e43a495461bb0
adeloing <adeloing@gmail.com>
2023-03-22 23:26:05 +0100

    "add utils file for clarity"

--- src/components/Collection/Collection.jsx
+++ src/components/Collection/Collection.jsx
@@ -1,220 +1,42 @@
 import { v1 as uuidv1 } from "uuid"
-import { Alchemy } from "alchemy-sdk"
 import "./Collection.css"
-import { statistiques } from "../../statistiques/statistiques"
-import mistery from "../../assets/images/mini_collection/NFT_MYSTERY.webp"
-import NFT1 from "../../assets/images/mini_collection/TPH_01.webp"
-import NFT2 from "../../assets/images/mini_collection/TPH_02.webp"
-import NFT3 from "../../assets/images/mini_collection/TPH_03.webp"
-import NFT4 from "../../assets/images/mini_collection/TPH_04.webp"
-import NFT5 from "../../assets/images/mini_collection/TPH_05.webp"
-import NFT6 from "../../assets/images/mini_collection/TPH_06.webp"
-import NFT7 from "../../assets/images/mini_collection/TPH_07.webp"
-import NFT8 from "../../assets/images/mini_collection/TPH_08.webp"
-import NFT9 from "../../assets/images/mini_collection/TPH_09.webp"
-import NFT10 from "../../assets/images/mini_collection/TPH_010.webp"
-import NFT11 from "../../assets/images/mini_collection/TPH_011.webp"
-import NFT12 from "../../assets/images/mini_collection/TPH_012.webp"
-import NFT13 from "../../assets/images/mini_collection/TPH_013.webp"
-import NFT14 from "../../assets/images/mini_collection/TPH_014.webp"
-import NFT15 from "../../assets/images/mini_collection/TPH_015.webp"
-import NFT16 from "../../assets/images/mini_collection/TPH_016.webp"
-import NFT17 from "../../assets/images/mini_collection/TPH_017.webp"
-import NFT18 from "../../assets/images/mini_collection/TPH_018.webp"
-import NFT19 from "../../assets/images/mini_collection/TPH_019.webp"
-import NFT20 from "../../assets/images/mini_collection/TPH_020.webp"
-import NFT21 from "../../assets/images/mini_collection/TPH_021.webp"
-import NFT22 from "../../assets/images/mini_collection/TPH_022.webp"
-import NFT23 from "../../assets/images/mini_collection/TPH_023.webp"
-import NFT24 from "../../assets/images/mini_collection/TPH_024.webp"
-import NFT25 from "../../assets/images/mini_collection/TPH_025.webp"
-import NFT26 from "../../assets/images/mini_collection/TPH_026.webp"
-import NFT27 from "../../assets/images/mini_collection/TPH_027.webp"
-import NFT28 from "../../assets/images/mini_collection/TPH_028.webp"
-import NFT29 from "../../assets/images/mini_collection/TPH_029.webp"
-import NFT30 from "../../assets/images/mini_collection/TPH_030.webp"
-import NFT31 from "../../assets/images/mini_collection/TPH_031.webp"
-import NFT32 from "../../assets/images/mini_collection/TPH_032.webp"
-import NFT33 from "../../assets/images/mini_collection/TPH_033.webp"
-import NFT34 from "../../assets/images/mini_collection/TPH_034.webp"
-import NFT35 from "../../assets/images/mini_collection/TPH_035.webp"
-import NFT36 from "../../assets/images/mini_collection/TPH_036.webp"
-import NFT37 from "../../assets/images/mini_collection/TPH_037.webp"
-import NFT38 from "../../assets/images/mini_collection/TPH_038.webp"
-import NFT39 from "../../assets/images/mini_collection/TPH_039.webp"
-import NFT40 from "../../assets/images/mini_collection/TPH_040.webp"
-import NFT41 from "../../assets/images/mini_collection/TPH_041.webp"
-import NFT42 from "../../assets/images/mini_collection/TPH_042.webp"
-import NFT43 from "../../assets/images/mini_collection/TPH_043.webp"
-import NFT44 from "../../assets/images/mini_collection/TPH_044.webp"
-import NFT45 from "../../assets/images/mini_collection/TPH_045.webp"
-import NFT46 from "../../assets/images/mini_collection/TPH_046.webp"
-import NFT47 from "../../assets/images/mini_collection/TPH_047.webp"
-import NFT48 from "../../assets/images/mini_collection/TPH_048.webp"
-import NFT49 from "../../assets/images/mini_collection/TPH_049.webp"
-import NFT50 from "../../assets/images/mini_collection/TPH_050.webp"
-
-const randomized = [
-    17, 20, 18, 37, 1, 19, 23, 5, 48, 32,
-    21, 7, 40, 16, 9, 42, 11, 24, 30, 10,
-    43, 45, 31, 39, 14, 6, 26, 15, 25, 2,
-    27, 4, 35, 13, 50, 49, 44, 22, 41, 29,
-    12, 47, 38, 46, 8, 36, 33, 3, 34, 28
-]
-
-const network = {
-    80001: "polygon-mumbai",
-    137: "polygon-mainnet",
-    1: "eth-mainnet",
-}
-
-const settings = {
-    apiKey: import.meta.env.VITE_MORALIS_API_KEY,
-    network: network["80001"],
-}
-
-const alchemy = new Alchemy(settings)
-
-const getList = async () => {
-    const collection = await alchemy.nft.getNftsForContract(
-        "0xd2A71deE05cF8f72b7F9Df45eD0CE481d5e41A1b"
-    )
-    const list_nft = [
-        NFT1, NFT2, NFT3, NFT4, NFT5, NFT6, NFT7, NFT8, NFT9, NFT10,
-        NFT11, NFT12, NFT13, NFT14, NFT15, NFT16, NFT17, NFT18, NFT19, NFT20,
-        NFT21, NFT22, NFT23, NFT24, NFT25, NFT26, NFT27, NFT28, NFT29, NFT30,
-        NFT31, NFT32, NFT33, NFT34, NFT35, NFT36, NFT37, NFT38, NFT39, NFT40,
-        NFT41, NFT42, NFT43, NFT44, NFT45, NFT46, NFT47, NFT48, NFT49, NFT50
-    ]
-    const list_1 = list_nft.slice(0, collection.nfts.length)
-    const list_2 = Array(50 - collection.nfts.length).fill(mistery)
-    const list_3 = [...list_1, ...list_2]
-    return list_3
-}
-
-const getTooltipList = async () => {
-  const collection = await alchemy.nft.getNftsForContract("0xd2A71deE05cF8f72b7F9Df45eD0CE481d5e41A1b")
-  const traitList = randomized.map((x, i) => {
-    return collection.nfts.length >= i ? collection.nfts[i] ? collection.nfts[i].rawMetadata.attributes : "???" : "???";
-  });
-
-  let result = [];
-  const metadata = traitList.map((x, i) => {
-    let res = [];
-    if (collection.nfts.length >= i) {
-      if (collection.nfts[i]) {
-        x.forEach((hash) => res.push(hash.value)); // hash.trait_type for attributes
-      }
-      else {
-        res.push("???");
-      }
-    }
-    let attributes = {
-      "GENRE" : res[0],
-      "LANGUAGE" : res[1],
-      "EXPRESSION" : res[2],
-      "GLASSES" : res[3],
-      "LISERE_TPH" : res[4],
-      "STICKER" : res[5],
-      "ARTICLE" : res[6],
-      "BACKGROUND_COLOR" : res[7],
-      "COMPUTER" : res[8]
-    }
-    result.push(attributes);
-    return result;
-  });
-  return metadata;
-}
-
-const metadata = await getTooltipList()
+import { getRandomizedArray, getList, getTooltipList } from "../../utils/utils.js"
 
 const list = await getList()
+let attributes = await getTooltipList()
 
 export const Collection =() => {
 
-  const howManyGenre = (value) => { return value ? statistiques.GENRE[0][value]*2 : "??";}
-  const howManyLanguage = (value) => { return value ? statistiques.LANGUAGE[0][value]*2 : "??";}
-  const howManyExpression = (value) => { return value ? statistiques.EXPRESSION[0][value]*2 : "??";}
-  const howManyGlasses = (value) => { return value ? statistiques.GLASSES[0][value]*2 : "??";}
-  const howManyLisereTph = (value) => { return value ? statistiques.LISERE_TPH[0][value]*2 : "??";}
-  const howManySticker = (value) => { return value ? statistiques.STICKER[0][value]*2 : "??";}
-  const howManyArticle = (value) => { return value ? statistiques.ARTICLE[0][value]*2 : "??";}
-  const howManyBackgroundColor = (value) => { return value ? statistiques.BACKGROUND_COLOR[0][value]*2 : "??";}
-  const howManyComputer = (value) => { return value ? statistiques.COMPUTER[0][value]*2 : "??";}
-
-  const getTooltipContent = (attributes) => {
-  
-    return (
-      <>
-        {attributes.map(({ label, value, count }) => {
-
-          const rarityClass = 
-            count === 2 ? "orange" : 
-            count >= 2  && count <= 8 ? "purple" : 
-            count >= 10 && count <= 18 ? "blue" : 
-            count >= 20 && count <= 36 ? "green" : 
-            count > 36 ? "gray" :
-            "";
-
-          return (
-            <div key={label}>
-              <span className="attribut">{label} : &nbsp; </span>
-              <span className="value">{value}&nbsp; &nbsp;</span>
-              { value !== "???" && (
-                <>
-                  <span className={rarityClass}>{count}</span>
-                  <span className="pourcent"> %</span>
-                </>
-              )}
-              <br />
-            </div>
-          );
-        })}
-      </>
-    );
+  const getTooltipContent = (index) => {
+    console.log(index)
+    let toolTip = attributes[index].map((x) => {
+      const id = uuidv1()
+      console.log("attribut: " + x.traitName+ ",value: " + x.traitValue + ",percent: " + x.traitPercent + ",color: " + x.traitColor)
+      return (
+        <div key={id}>
+          <span className="attribut">{x.traitName} : &nbsp; </span>
+          <span className="value">{x.traitValue}&nbsp; &nbsp;</span>
+          <span className={x.traitColor}>{x.traitPercent}</span>
+          <span className="pourcent"> %</span>
+          <br />
+        </div>
+      )
+    })
+    return toolTip
   }
 
   const imgTab = (slide, nameClass, start, end) => {
 
-    for (let i = start; i <= end; i++) {
-      const metadataMap = metadata.map((item) => item[randomized[i-1] - 1]);
-      const img = list[randomized[i - 1] - 1];
+    for (let i = start; i < end; i++) {
+      const rid = getRandomizedArray()[i - 1]
+      const img = list[rid];
       const id = uuidv1();
-      const meta = metadataMap[i - 1];
-      const genre = meta.GENRE ? meta.GENRE : "???";
-      const language = meta.LANGUAGE ? meta.LANGUAGE : "???";
-      const expression = meta.EXPRESSION ? meta.EXPRESSION : "???";
-      const glasses = meta.GLASSES ? meta.GLASSES : "???";
-      const lisereTph = meta.LISERE_TPH ? meta.LISERE_TPH : "???";
-      const sticker = meta.STICKER ? meta.STICKER : "???";
-      const article = meta.ARTICLE ? meta.ARTICLE : "???";
-      const backgroundColor = meta.BACKGROUND_COLOR ? meta.BACKGROUND_COLOR : "???";
-      const computer = meta.COMPUTER ? meta.COMPUTER : "???";
-      const genreCount = meta.GENRE ? howManyGenre(meta.GENRE) : "??";
-      const languageCount = meta.LANGUAGE ? howManyLanguage(meta.LANGUAGE) : "??";
-      const expressionCount = meta.EXPRESSION ? howManyExpression(meta.EXPRESSION) : "??";
-      const glassesCount = meta.GLASSES ? howManyGlasses(meta.GLASSES) : "??";
-      const lisereTphCount = meta.LISERE_TPH ? howManyLisereTph(meta.LISERE_TPH) : "??";
-      const stickerCount = meta.STICKER ? howManySticker(meta.STICKER) : "??";
-      const articleCount = meta.ARTICLE ? howManyArticle(meta.ARTICLE) : "??";
-      const backgroundColorCount = meta.BACKGROUND_COLOR ? howManyBackgroundColor(meta.BACKGROUND_COLOR) : "??";
-      const computerCount = meta.COMPUTER ? howManyComputer(meta.COMPUTER) : "??";
   
       slide.push(
         <div key={id} className={nameClass}>
           <img src={img} height="200" width="200" alt={`NFT_${i}`} />
           <span className="tooltiptext">
-            {getTooltipContent([
-              { label: "GENRE", value: genre, count: genreCount },
-              { label: "LANGUAGE", value: language, count: languageCount },
-              { label: "EXPRESSION", value: expression, count: expressionCount },
-              { label: "GLASSES", value: glasses, count: glassesCount },
-              { label: "LISERE_TPH", value: lisereTph, count: lisereTphCount },
-              { label: "STICKER", value: sticker, count: stickerCount },
-              { label: "ARTICLE", value: article, count: articleCount },
-              { label: "BG_COLOR", value: backgroundColor, count: backgroundColorCount },
-              { label: "COMPUTER", value: computer, count: computerCount },
-            ])}
+            {getTooltipContent(rid)}
           </span>
         </div>
       );
