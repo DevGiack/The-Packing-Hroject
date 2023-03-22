371448b3c9cfc08a2bc5cb57a5c3b23e3a19288b
YannGUIOT <guiot_yann@hotmail.fr>
2023-03-22 19:17:48 +0100

    add details values + css update

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -34,7 +34,15 @@ export const Details = () => {
 
     const currentNFT = nftsForOwner.find(nft => nft.tokenId === TokenId)
     const currentImgUrl = currentNFT?.media[0]?.gateway;
-
+    const currentGenre = currentNFT?.rawMetadata?.attributes[0]?.value;
+    const currentLanguage = currentNFT?.rawMetadata?.attributes[1]?.value;
+    const currentExpression = currentNFT?.rawMetadata?.attributes[2]?.value;
+    const currentGlasses = currentNFT?.rawMetadata?.attributes[3]?.value;
+    const currentLisereTPH = currentNFT?.rawMetadata?.attributes[4]?.value;
+    const currentSticker = currentNFT?.rawMetadata?.attributes[5]?.value;
+    const currentArticle = currentNFT?.rawMetadata?.attributes[6]?.value;
+    const currentBackground = currentNFT?.rawMetadata?.attributes[7]?.value;
+    const currentComputer = currentNFT?.rawMetadata?.attributes[8]?.value;
     return (
     <>
         {currentImgUrl && (
@@ -48,28 +56,19 @@ export const Details = () => {
             />
             </div>
             <div className="metadata_details">
-                GENRE : MEN TYPE 2 LANGUAGE : HTML/CSS
-                <br></br>
-                EXPRESSION : N°3
-                <br></br>
-                GLASSES : NO GLASSES
-                <br></br>
-                LISERE TPH : CLASSIC
-                <br></br>
-                STICKER : STACK OVERFLOW
-                <br></br>
-                ARTICLE : POSTER
-                <br></br>
-                BACKGROUND COLOR : BROWN
-                <br></br>
-                COMPUTER : SILVER
+                <span className="attrib">ID : <span className="values"> #{TokenId}</span></span><br />
+                <span className="attrib">GENRE : <span className="values"> {currentGenre}</span></span><br />
+                <span className="attrib">LANGUAGE : <span className="values"> {currentLanguage}</span></span><br />
+                <span className="attrib">EXPRESSION : <span className="values"> {currentExpression}</span></span><br />
+                <span className="attrib">GLASSES : <span className="values"> {currentGlasses}</span></span><br />
+                <span className="attrib">LISERE TPH : <span className="values"> {currentLisereTPH}</span></span><br />
+                <span className="attrib">STICKER : <span className="values"> {currentSticker}</span></span><br />
+                <span className="attrib">ARTICLE : <span className="values"> {currentArticle}</span></span><br />
+                <span className="attrib">BG COLOR : <span className="values"> {currentBackground}</span></span><br />
+                <span className="attrib">COMPUTER : <span className="values"> {currentComputer}</span></span>
             </div>
         </div>
         )}
     </>
     );
-
-
-
-
 }
