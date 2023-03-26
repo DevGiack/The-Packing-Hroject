915a39f53b6d6d1ce433034c42439eeb62bf70c7
YannGUIOT <guiot_yann@hotmail.fr>
2023-03-26 12:18:04 +0200

    add error details page + rectif adress & css

--- src/components/Collection/Collection.jsx
+++ src/components/Collection/Collection.jsx
@@ -1,6 +1,6 @@
+import { getRandomizedArray, getList, getTooltipList } from "../../utils/utils.js"
 import { v1 as uuidv1 } from "uuid"
 import "./Collection.css"
-import { getRandomizedArray, getList, getTooltipList } from "../../utils/utils.js"
 
 const list = await getList()
 let attributes = await getTooltipList()
